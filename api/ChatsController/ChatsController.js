169b7c02ac6cf45d4bf8e24d275cfbac62d36d7c
LukeFj <112558889+LukeFj@users.noreply.github.com>
2024-11-04 08:36:15 +0200

    Added Chats Controller

--- api/ChatsController/ChatsController.js
+++ api/ChatsController/ChatsController.js
@@ -1,8 +1,16 @@
+import api from "../apiconfig";
 
 async function chatHistory(username) {
-
 return new Promise((resolve,reject)=>{
-    
-})
-    
+    api.post("/chats/history",{username}).then((response)=>{
+        if(response.status === 200){
+            resolve(response.data.chats);
+        }
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}) 
 }
+
+export {chatHistory};
