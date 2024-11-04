d1bd92fe0dfd7c0097a95f8178637601b00ec233
LukeFj <112558889+LukeFj@users.noreply.github.com>
2024-11-04 07:44:40 +0200

    Added Cors

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const app = express();
 const PORT = process.env.PORT;
 const verifyToken = require("./auth/jwtAuth");
 const authRoutes = require("./router/authRoutes");
+const cors = require("cors");
 
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
